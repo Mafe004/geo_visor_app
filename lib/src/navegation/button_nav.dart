236f8d7267e35eaabbffb4329e1957cf17d82d22
maria <mariaferco94@gmail.com>
2024-03-31 03:42:23 -0500

    config2

--- lib/src/navegation/button_nav.dart
+++ lib/src/navegation/button_nav.dart
@@ -8,10 +8,19 @@ class Bnavigator extends StatefulWidget {
 }
 
 class _BnavigatorState extends State<Bnavigator> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: index,
+      onTap: (int i){
+        setState(() {
+
+        });
+        index = i;
+      },
       type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.indigoAccent,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
