fcf488f8da659b2df80b126b1391bb578d58e4a1
maria <mariaferco94@gmail.com>
2024-03-31 02:01:41 -0500

    Form

---
+++ lib/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:geo_visor_app/form.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(4.874280, -74.087537);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Geo Visor A&S'),
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FormExampleApp()),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geo_visor_app/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(4.874280, -74.087537);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Geo Visor A&S '),
-          backgroundColor: Colors.indigoAccent
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+      title: 'Geo Visor A&S',
+      home: const HomeScreen(),
     );
   }
 }
