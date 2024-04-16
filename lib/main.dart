1540457601a428613d4cbd59c351a47314b7ad99
maria <mariaferco94@gmail.com>
2024-04-15 20:36:52 -0500

    FirebaseCLIlogin

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:geo_visor_app/src/features/navegation/button_nav.dart';
+import 'package:geo_visor_app/src/routing/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:geo_visor_app/src/navegation/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,4 +21,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          child: Text('Go to HomePage'),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  Bnavigator? myBnb;
+
+  @override
+  void initState() {
+    myBnb = Bnavigator(currentIndex: (i) {
+      setState(() {
+        index = i;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: myBnb,
+      body: Routes(index: index),
+    );
+  }
+}
 
